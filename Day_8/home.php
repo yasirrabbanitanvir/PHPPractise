8afbe1e301c9e0990eaccba4465ec2e5c6f57f7c
Yasir Rabbani Tanvir <yasirrabbanitanvir@gmail.com>
2025-01-08 19:06:56 +0600

    update

---
+++ Day_8/home.php
@@ -0,0 +1,9 @@
+<?php
+
+$nam = $_REQUEST['username'];
+$em = $_REQUEST['email'];
+$pass = $_REQUEST['password'];
+
+echo strlen($pass);
+
+?>
---
+++ Day_8/index.php
@@ -0,0 +1,8 @@
+<form action="home.php" method="post">
+    
+<input type="text" name="username" placeholder="Enter your name" required>
+<input type="email" name="email" placeholder="Enter your email" required>
+<input type="password" name="password" placeholder="Enter your password" required>
+<input type="submit" value="submit" >
+
+</form>
