ddecb6b0e22797a327fe19a25cd85ad86d68c1f7
Yasir Rabbani Tanvir <yasirrabbanitanvir@gmail.com>
2025-01-09 09:34:00 +0600

    update

--- Day_8/home.php
+++ Day_8/home.php
@@ -1,9 +1,14 @@
 <?php
-
 $nam = $_REQUEST['username'];
 $em = $_REQUEST['email'];
 $pass = $_REQUEST['password'];
 
-echo strlen($pass);
+$countPass = strlen($pass);
 
+if (!($countPass >= 5 && $countPass <= 10)) {
+    header("Location: index.php?wrongPass=Your password must be between 5 and 10 characters long.");   
+} 
+else {
+    echo "Perfect";
+}
 ?>
